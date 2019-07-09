bd630970814cc1718fa99717cf99ccbd57ee9d4f
Elen <elen.grigorian96@gmail.com>
2019-07-09 19:02:48 +0400

    Fixed eslint warnings

--- src/Item.js
+++ src/Item.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 
-export const Item = (props) => {
-    return <div id={`item_${props.index}`} style={{backgroundColor: props.backgroundColor}}>
-            <span id="buttons">
-            <button type="button" onClick={props.addItem}>Add Item</button>
-            <button type="button" onClick={()=> props.removeItem(props.index)}>Remove</button>
-            </span>
-            </div>
-}
+export const Item = props => <div id={`item_${props.index}`} style={{ backgroundColor: props.backgroundColor }}>
+  <span id='buttons'>
+    <button type='button' onClick={props.addItem}>Add Item</button>
+    <button type='button' onClick={() => props.removeItem(props.index)}>Remove</button>
+  </span>
+</div>;
 export default Item;
